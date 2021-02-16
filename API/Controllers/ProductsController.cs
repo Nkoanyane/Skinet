db615f32a92303351d937e4b48fca1c927c6483f
Nkoanyane Makateng <nmakateng@cbl.org.ls>
2021-02-16 21:30:56 +0200

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -12,23 +13,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext Context;
-        public ProductsController(StoreContext Context)
+        private readonly IProductRepository repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            this.Context = Context;
+            this.repo = repo;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> getProducts()
         {
-            var Products = await this.Context.Products.ToListAsync();
+            var Products = await this.repo.getProductsAsync();
             return Ok(Products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> getProduct(int id)
         {
-            return await this.Context.Products.FindAsync(id);
+            return await this.repo.getProductByIDAsync(id);
         }
+        [HttpGet("brands")]
+       public async Task<ActionResult<IReadOnlyList<ProductBrand>>> getProductBrands()
+        {
+            return Ok(await this.repo.getProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await this.repo.GetProductTypesAsync());
+        }
+        
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +11,28 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host= CreateHostBuilder(args).Build();
+
+           using (var Scope= host.Services.CreateScope())
+           {
+               var services =Scope.ServiceProvider;
+               var LoggerFactory=services.GetRequiredService<ILoggerFactory>();
+               try
+               {
+                   var context=services.GetRequiredService<StoreContext>();
+                   await context.Database.MigrateAsync();
+                   await StoreContextSeed.SeedAsync(context,LoggerFactory);
+               }
+               catch (Exception ex)
+               {
+                   var logger=LoggerFactory.CreateLogger<Program>();
+                   logger.LogError(ex,"An error occurred during Migration");
+               }
+           }
+           host.Run();
+    
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
